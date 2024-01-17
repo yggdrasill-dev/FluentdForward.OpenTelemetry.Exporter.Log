47ff6721a65f9f3d75561419048a24dc2e18365f
Phoenix <phoenix.chen@rayprosoft.com>
2024-01-17 17:59:44 +0800

    調整 ETW 輸出訊息，以方便錄 Log Message

--- FluentdForward.OpenTelemetry.Exporter.Logs/FluentdForwardExporterEventSource.cs
+++ FluentdForward.OpenTelemetry.Exporter.Logs/FluentdForwardExporterEventSource.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.Tracing;
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Globalization;
 
 namespace FluentdForward.OpenTelemetry.Exporter.Logs;
 
@@ -7,18 +9,36 @@ internal class FluentdForwardExporterEventSource : EventSource
 {
 	public static readonly FluentdForwardExporterEventSource Log = new();
 
-	[Event(1, Level = EventLevel.Informational, Keywords = Keywords.Startup)]
-	public void ExportRegister()
-	{
-		if (IsEnabled(EventLevel.Informational, Keywords.Startup))
-			WriteEvent(1, "FluentdForward export set.");
-	}
-
-	[Event(4, Message = "Unknown error in export method: {0}", Level = EventLevel.Error, Keywords = Keywords.Export)]
+	[NonEvent]
 	public void ExportMethodException(Exception ex)
 	{
 		if (IsEnabled(EventLevel.Error, Keywords.Export))
-			WriteEvent(4, ex.ToString());
+		{
+			var originalUICulture = Thread.CurrentThread.CurrentUICulture;
+
+			try
+			{
+				Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+				ExportMethodException(ex.ToString());
+			}
+			finally
+			{
+				Thread.CurrentThread.CurrentUICulture = originalUICulture;
+			}
+		}
+	}
+
+	[Event(1, Level = EventLevel.Informational, Keywords = Keywords.Startup, Message = "FluentdForward export set.")]
+	public void ExportRegister()
+	{
+		if (IsEnabled(EventLevel.Informational, Keywords.Startup))
+			WriteEvent(1);
+	}
+
+	[Event(4, Message = "Unknown error in export method: {0}", Level = EventLevel.Error, Keywords = Keywords.Export)]
+	public void ExportMethodException(string ex)
+	{
+		WriteEvent(4, ex.ToString());
 	}
 
 	public static class Keywords
